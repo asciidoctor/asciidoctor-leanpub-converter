fb5ada4fc8e3e3a937ea02c470c4b9b17c1f4a61
Schalk W. Cronjé <ysb33r@gmail.com>
2015-06-03 17:00:41 +0200

    FIXED #28 - Caution & Important Admonitions

--- core/src/test/groovy/org/ysb33r/asciidoctor/leanpub/AdmonitionsSpec.groovy
+++ core/src/test/groovy/org/ysb33r/asciidoctor/leanpub/AdmonitionsSpec.groovy
@@ -26,7 +26,6 @@ W> This is a warning admonition.
 W>   And some more text.
 
 '''
-
     }
 
     @Issue('https://leanpub.com/help/manual#leanpub-auto-warning')
@@ -46,7 +45,32 @@ W> ## Warning with title
 W> This is a warning with title
 
 '''
+    }
 
+    @Issue('https://github.com/ysb33r/asciidoctor-leanpub-converter/issues/28')
+    def "Caution and Important"() {
+        setup:
+        File chapter = new File(manuscriptDir,'chapter_3.txt')
+
+        when:
+        generateOutput('admonitions.adoc')
+
+        then:
+        chapter.text == '''# Caution and Important
+
+{icon=fire}
+G> This is a caution
+
+
+Some text in between
+
+{icon=university}
+G> ## Yip, really important
+G> This is important text
+
+
+Some text afterwards
+'''
     }
 
 }
