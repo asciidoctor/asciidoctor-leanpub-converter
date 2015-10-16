bd276f6de58d7f3b6ff08f2dddffaa03cdf55968
Schalk W. Cronjé <ysb33r@gmail.com>
2015-10-16 16:33:31 +0100

    Major refactoring. All converters and core core code is now in one jar. Multiple services will be made available from the single jar

---
+++ converters/src/test/groovy/org/asciidoctor/markdown/BasicMarkdownSpec.groovy
@@ -0,0 +1,53 @@
+package org.asciidoctor.markdown
+
+import org.asciidoctor.markdown.internal.MarkdownSpecification
+import spock.lang.FailsWith
+import spock.lang.Ignore
+import spock.lang.Issue
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+/**
+ * @author Schalk W. Cronjé
+ */
+class BasicMarkdownSpec extends MarkdownSpecification {
+
+//    @Ignore
+    @Issue('https://leanpub.com/help/manual#leanpub-auto-numberedordered-lists, https://github.com/ysb33r/asciidoctor-leanpub-converter/issues/1')
+    @FailsWith(org.spockframework.runtime.ConditionNotSatisfiedError)
+    @Unroll
+    def "Converting #adocType (with issues)"() {
+
+        given:
+        def results = generateOutput( adocType+'.adoc' )
+
+        expect:
+        results.outputFile.exists()
+        results.outputFile.text == results.expectedFile.text
+
+        where:
+        adocType << [
+            'simple-book'
+        ]
+
+    }
+
+//    @Unroll
+//    def "Converting #adocType"() {
+//
+//        given:
+//        def results = generateOutput( adocType+'.adoc' )
+//
+//        expect:
+//        results.outputFile.exists()
+//        results.outputFile.text == results.expectedFile.text
+//
+//        where:
+//        adocType << [
+//            'simple-book'
+//        ]
+//
+//    }
+
+}
